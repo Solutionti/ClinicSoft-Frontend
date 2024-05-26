ff704038c124abbff9f4ddd88902586f543ff6a8
Desarrollador Jerson Galvez <desarrollo.gocho@gmail.com>
2024-05-25 19:49:47 -0500

    subiendo los cambios

--- src/app/admisiones/admision/admision.component.ts
+++ src/app/admisiones/admision/admision.component.ts
@@ -25,23 +25,22 @@ export class AdmisionComponent implements OnInit {
 
   admisionForm: FormGroup = new FormGroup({
     dni_admision: new FormControl(''),
-    hc_admision: new FormControl(''),
+    hc_admision: new FormControl({value:'', disabled: true}),
     si_admision: new FormControl(''),
-    nombre_admision: new FormControl(''),
+    nombre_admision: new FormControl({value:'', disabled: true}),
     especialidad_admision: new FormControl(''),
     doctor_admision: new FormControl(''),
   });
 
   admisionForm2: FormGroup = new FormGroup({
-    fecha_admision: new FormControl(''),
-    factura_admision: new FormControl(''),
-    costo_admision: new FormControl(''),
+    fecha_admision: new FormControl({value:'', disabled: true}),
+    factura_admision: new FormControl({value:'', disabled: true}),
+    costo_admision: new FormControl({value:'', disabled: true}),
     descuento_admision: new FormControl(''),
     comision_admision: new FormControl(''),
     recibida_admision: new FormControl(''),
-    devolver_admision: new FormControl(''),
-    efectivo_admision: new FormControl(''),
-    tarjeta_admision: new FormControl(''),
+    devolver_admision: new FormControl({value:'', disabled: true}),
+    efectivo_admision: new FormControl('0'),
     total_admision: new FormControl('')
   });
 
