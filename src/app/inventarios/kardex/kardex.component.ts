3e292b204aac951b4fee44a55313b34c0407d0a3
Cristian Vargas <ccvargas95@gmail.com>
2024-05-29 23:36:36 -0500

    subiendo Cambios

--- src/app/inventarios/kardex/kardex.component.ts
+++ src/app/inventarios/kardex/kardex.component.ts
@@ -1,9 +1,13 @@
+import { ListasService } from './../../services/listas.service';
+import { InventarioService } from './../services/inventario.service';
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { MenuComponent } from '../../componentes/menu/menu.component';
 import { CerrarsesionComponent } from '../../componentes/cerrarsesion/cerrarsesion.component';
 import { RouterOutlet } from '@angular/router';
 import { TableModule } from 'primeng/table';
+import internal from 'stream';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-kardex',
@@ -13,19 +17,67 @@ import { TableModule } from 'primeng/table';
     ReactiveFormsModule,
     MenuComponent,
     CerrarsesionComponent,
-    TableModule
+    TableModule,
+    CommonModule
   ],
   templateUrl: './kardex.component.html'
 })
 export class KardexComponent implements OnInit {
 
+  constructor(
+    private InventarioService: InventarioService,
+    private ListasService: ListasService
+  ){}
+
   ngOnInit(): void {
 
+    this.getProducts();
+   this.getCategories();
   }
 
-  kardexForm = new FormGroup({
-    producto_kardex: new FormControl(''),
-    inicial_kardex: new FormControl(''),
-    final_kardex: new FormControl(''),
+  btnPdfHidden = true;
+
+  kardexForm: FormGroup = new FormGroup({
+    categoria_kardex: new FormControl('',[Validators.required]),
+    producto_kardex: new FormControl('',[Validators.required]),
+    inicial_kardex: new FormControl('',[Validators.required]),
+    final_kardex: new FormControl('',Validators.required),
   });
+
+  get categoriaControl(): FormControl {
+    return this.kardexForm.get('categoria_kardex') as FormControl;
+  }
+
+  get productoControl(): FormControl {
+    return this.kardexForm.get('producto_kardex') as FormControl;
+  }
+
+  get inicialControl(): FormControl {
+    return this.kardexForm.get('inicial_kardex') as FormControl;
+  }
+
+  get finalControl(): FormControl {
+    return this.kardexForm.get('final_kardex') as FormControl;
+  }
+
+  getProduct: any [] = [];
+  getProducts(){
+
+    this.InventarioService
+        .getProducts()
+        .subscribe((response: any) =>{
+          console.log(response);
+          this.getProduct = response;
+    });
+  }
+  getCategorie: any [] = []
+  getCategories() {
+
+    this.ListasService
+        .getCategories()
+        .subscribe((response: any ) => {
+          console.log(response);
+          this.getCategorie = response
+        });
+  }
 }
