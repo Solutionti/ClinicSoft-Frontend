4f71647ba1f4535f94ab8328e4467ac7d03013f1
Desarrollador Jerson Galvez <desarrollo.gocho@gmail.com>
2024-05-30 22:35:20 -0500

    subiendo los cambios

--- src/app/inventarios/services/inventario.service.ts
+++ src/app/inventarios/services/inventario.service.ts
@@ -16,4 +16,22 @@ export class InventarioService {
 
     return this.http.get(url);
   }
+
+  // CREACION DEL SERVICIO DE CREAR EL PRODUCTO 
+  createProduct(datos: any) {
+    const url = `${environment.apiClinicSoft}createProduct`;
+
+    return this.http.post(url, {
+      categoria: datos[0].categoria,
+      nombre: datos[0].nombre,
+      codigo: datos[0].codigo,
+      barras: datos[0].barras,
+      medida: datos[0].medida,
+      cantidad: datos[0].cantidad,
+      precio_u: datos[0].precio_u,
+      moneda: datos[0].moneda,
+      descripcion: datos[0].descripcion,
+      usuario: datos[0].usuario,
+    });
+  }
 }
