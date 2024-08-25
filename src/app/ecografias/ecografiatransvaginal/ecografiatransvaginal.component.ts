c11865b1a68673bb3128d534748a301f18feaaa3
Desarrollador Jerson Galvez <desarrollo.gocho@gmail.com>
2024-08-25 11:24:42 -0500

    subiendo las carpetas de los archivos de eocgrafias

---
+++ src/app/ecografias/ecografiatransvaginal/ecografiatransvaginal.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-ecografiatransvaginal',
+  standalone: true,
+  imports: [],
+  templateUrl: './ecografiatransvaginal.component.html',
+  styleUrl: './ecografiatransvaginal.component.css'
+})
+export class EcografiatransvaginalComponent {
+
+}
