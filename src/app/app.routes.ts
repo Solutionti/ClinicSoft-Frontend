484910a5deca4fa9f361bc7f21b508c619d7e814
Desarrollador Jerson Galvez <desarrollo.gocho@gmail.com>
2024-08-25 11:18:08 -0500

    subiendo los cambios

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,22 +1,4 @@
 import { Routes } from '@angular/router';
-import { IniciarsesionComponent } from './authorization/iniciarsesion/iniciarsesion.component';
-import { PacientesComponent } from './admisiones/pacientes/pacientes.component';
-import { TriageComponent } from './admisiones/triage/triage.component';
-import { AdmisionComponent } from './admisiones/admision/admision.component';
-import { LaboratorioComponent } from './admisiones/laboratorio/laboratorio.component';
-import { ColposcopiaComponent } from './procedimientos/colposcopia/colposcopia.component';
-import { EcografiaComponent } from './ecografias/ecografia/ecografia.component';
-import { InventarioComponent } from './inventarios/inventario/inventario.component';
-import { KardexComponent } from './inventarios/kardex/kardex.component';
-import { UsuariosComponent } from './authorization/usuarios/usuarios.component';
-import { HistorialpacienteComponent } from './procedimientos/historialpaciente/historialpaciente.component';
-import { PagosComponent } from './contabilidad/pagos/pagos.component';
-import { GastosComponent } from './contabilidad/gastos/gastos.component';
-import { FinanzasComponent } from './contabilidad/finanzas/finanzas.component';
-import { ReporteComponent } from './reportes/reporte/reporte.component';
-import { InicioComponent } from './authorization/inicio/inicio.component';
-import { ProductosComponent } from './inventarios/productos/productos.component';
-import { CitasComponent } from './admisiones/citas/citas.component';
 
 export const routes: Routes = [
   {
@@ -107,11 +89,7 @@ export const routes: Routes = [
     loadComponent: () =>
       import('./inventarios/productos/productos.component').then((c) => c.ProductosComponent)
   },
-  {
-    path: 'doctores',
-    loadComponent: () =>
-      import('./authorization/doctores/doctores.component').then((c) => c.DoctoresComponent)
-  },
+  
   {
     path: 'ecografias',
     loadComponent: () =>
