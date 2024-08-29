ab8dab4de20d8862a88e67caf865d03a48fa9f6b
Desarrollador Jerson Galvez <desarrollo.gocho@gmail.com>
2024-08-29 09:50:53 -0500

    subiendo los cambios

--- src/app/admisiones/citas/citas.component.ts
+++ src/app/admisiones/citas/citas.component.ts
@@ -1,50 +1,109 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { CerrarsesionComponent } from '../../componentes/cerrarsesion/cerrarsesion.component';
 import { MenuComponent } from '../../componentes/menu/menu.component';
-import { FullCalendarModule } from '@fullcalendar/angular';
-import { CalendarOptions } from '@fullcalendar/core';
+
+import { FullCalendarComponent, FullCalendarModule } from '@fullcalendar/angular';
+import { Calendar, CalendarOptions } from '@fullcalendar/core';
 import dayGridPlugin from '@fullcalendar/daygrid';
 import interactionPlugin from '@fullcalendar/interaction';
 import listPlugin from '@fullcalendar/list';
 import allLocales from '@fullcalendar/core/locales-all';
+import timeGridPlugin from '@fullcalendar/timegrid'
+import { CalendarModule } from 'primeng/calendar';
+import { FormControl, FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { ListasService } from '../../services/listas.service';
+import { CommonModule } from '@angular/common';
+import { TableModule } from 'primeng/table';
+import { DialogModule } from 'primeng/dialog';
+import { DatePipe } from '@angular/common';
+
 @Component({
   selector: 'app-citas',
   standalone: true,
-  imports: [CerrarsesionComponent, MenuComponent, FullCalendarModule],
+  imports: [
+    CerrarsesionComponent,
+    MenuComponent,
+    FullCalendarModule,
+    CalendarModule,
+    FormsModule,
+    ReactiveFormsModule,
+    CommonModule,
+  ],
   templateUrl: './citas.component.html',
+  providers: [DatePipe]
 })
 
 export class CitasComponent implements OnInit  {
-
-  constructor() {
-
+  date: Date[] | undefined;
+  
+  constructor(
+    private listaServices: ListasService,
+    private datePipe: DatePipe
+  ) {
+   
   }
-
+  
   ngOnInit(): void {
-
+    this.getDoctores();
   }
 
+  calendarForm = new FormGroup({
+    calendar_input: new FormControl('')
+  });
+
+  RegistroCitasForm = new FormGroup({
+    
+  });
+
+  visible1 = true;
+
+  @ViewChild('calendar') calendarComponent: FullCalendarComponent | undefined;
   calendarOptions: CalendarOptions = {
     initialView: 'dayGridMonth',
-    plugins: [dayGridPlugin, interactionPlugin, listPlugin],
+    plugins: [dayGridPlugin, interactionPlugin, listPlugin,timeGridPlugin],
+    headerToolbar: {
+      left: 'prev,next',
+      center: 'title',
+      right: 'dayGridMonth,timeGridWeek,timeGridDay' // user can switch between the two
+    },
     locales: allLocales,
     locale: 'es',
-    editable: true,
+    editable: false,
     selectable: true,
-    dateClick: function(info) {
-      alert('Clicked on: ' + info.dateStr);
-      alert('Coordinates: ' + info.jsEvent.pageX + ',' + info.jsEvent.pageY);
-      alert('Current view: ' + info.view.type);
-      // change the day's background color just for fun
-      info.dayEl.style.backgroundColor = 'red';
+    themeSystem: 'bootstrap',
+    eventDisplay: 'block', 
+    events: function(fetchInfo, successCallback, failureCallback) {
+      fetch('http://localhost:8000/clinicsoft/getCitas')
+        .then(response => response.json())
+        .then(data => {
+          console.log(data);
+          successCallback(data);
+        })
+        .catch(error => {
+          failureCallback(error);
+        });
     },
-    events: [
-      { title: 'event 1', date: '2024-06-09' },
-      { title: 'event 2', date: '2024-06-08' },
-    ]
+    eventAdd: function(info) {
+      alert('Evento agregado: ' + info.event.title); 
+    }
   };
 
-  handleDateClick(arg: any ) {
-    alert('date click! ' + arg.dateStr)
+  getDoctor: any[] = [];
+  getDoctores(){
+    this.listaServices
+        .getDoctor()
+        .subscribe((response: any ) => {
+          this.getDoctor = response;
+          
+        });
+  }
+
+  addEvent() {
+    let fecha: any = this.calendarForm.get("calendar_input")?.value;
+    const date = new Date(fecha);
+
+    let transforDate = this.datePipe.transform(date, 'dd-MM-yyyy');
+
+    alert(transforDate);
   }
 }
