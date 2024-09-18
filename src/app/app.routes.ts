7b3fb02427ad03647b36f8f22ba696ac7796c2f5
caqi26 <99498976+caqi26@users.noreply.github.com>
2024-09-18 01:36:18 -0500

    ecos1809

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -132,4 +132,14 @@ export const routes: Routes = [
     loadComponent: () =>
       import('./ecografias/ecografiaabdominal/ecografiaabdominal.component').then((c) => c.EcografiaabdominalComponent)
   },
+  {
+    path: 'ecografiatiroides',
+    loadComponent: () =>
+      import('./ecografias/ecografiatiroides/ecografiatiroides.component').then((c) => c.EcografiatiroidesComponent)
+  },
+  {
+    path: 'ecografiahisterosonografia',
+    loadComponent: () =>
+      import('./ecografias/ecografiahisterosonografia/ecografiahisterosonografia.component').then((c) => c.EcografiahisterosonografiaComponent)
+  },
 ];
